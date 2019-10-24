eeaff8ecaae15069553b79ce2cae6ccedf5ebe5e
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2019-10-24 11:58:53 +0200

    Regex done and refactoring

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -20,10 +20,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 {
 
     private String input = "";
-    private final String ALL_SIMBOLS = "/*+=-.()";
+    private TextView tvInput = findViewById(R.id.textViewInput);
+    private TextView tvResult = findViewById(R.id.textViewResult);
     private final String OPERATION_SIMBOLS = "/*+-";
-    private final String SPECIAL_SIMBOLS = "-.";
-    private final String EVEN_MORE_SPECIAL_SIMBOLS = "()";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,8 +37,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             @Override
             public boolean onLongClick(View v) {
-                TextView tvInput = findViewById(R.id.textViewInput);
+
                 tvInput.setText("");
+                tvResult.setText("");
                 input = "";
                 return true;
             }
@@ -49,19 +50,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v)
     {
-        TextView tvResult = this.findViewById(R.id.textViewResult);
         Button button = (Button)v;
 
-        if(validateInput(button))
+        this.addInput(button);
+
+        try
         {
-            if((Pattern.matches("(^([-]?(\\d+)|(\\d+[-+*\\/]\\d+)+([-+*\\/]\\d?)?)+$)*", input)))
-            {
-                String calculation = Double.toString(calculate(input));
-                tvResult.setText(calculation);
-            }
+            String calculation = Double.toString(calculate(input));
+            this.tvResult.setText(calculation);
+        }catch(IllegalArgumentException e)
+        {
+            this.tvResult.setText("");
+        }catch(ArithmeticException e)
+        {
+            this.tvResult.setText("");
         }
-
-
     }
 
 
@@ -110,103 +113,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public boolean validateInput(View v)
+    public void addInput(View v)
     {
-        boolean val = false;
-        TextView tvInput = this.findViewById(R.id.textViewInput);
-        String strInput = (String) tvInput.getText();
+        String strInput = (String) this.tvInput.getText();
         Button button = (Button)v;
         String textBtn = (String) button.getText();
-        int inputLen = input.length();
+        int inputLen = this.input.length();
 
-        if (Pattern.matches( "\\d", textBtn))
+        if (Pattern.matches("^(-?\\d*(\\.\\d*)?(?<=\\d|\\.)[*\\/^+-]?)+$", strInput + textBtn))
         {
-            //Numeros
-            input += textBtn;
-            tvInput.setText(input);
-            val = true;
-        } else if (Pattern.matches("(^([-]?(\\d+)|(\\d+[-+*\\/]\\d+)+([-+*\\/]\\d?)?)+[-+*\\/]?$)*", strInput + textBtn))
-        {
-            if(OPERATION_SIMBOLS.contains(input.substring(inputLen-1)))
+            if(OPERATION_SIMBOLS.contains(inputLen == 0 ? "" : this.input.substring(inputLen-1)) &&
+                OPERATION_SIMBOLS.contains(textBtn))
             {
-                input = input.substring(0, inputLen-1) + textBtn;
-                tvInput.setText(input);
-                val = true;
+                this.input = this.input.substring(0, inputLen-1) + textBtn;
+                this.tvInput.setText(this.input);
             } else
             {
-                input += textBtn;
-                tvInput.setText(input);
-                val = true;
+                this.input += textBtn;
+                this.tvInput.setText(this.input);
             }
         } else if (textBtn.equals("DEL"))
         {
             //Boton DEL
-            input = input.length() == 0 ? "" : input.substring(0, inputLen - 1);
-            tvInput.setText(input);
-            val = true;
-        } /*else if (textBtn.equals("="))
-        {
-            calculate(input);
+            this.input = this.input.length() == 0 ? "" : this.input.substring(0, inputLen - 1);
+            this.tvInput.setText(this.input);
         }
 
-
-        /*
-
-        } else if (OPERATION_SIMBOLS.contains(
-            input.equals("") ? "" : input.substring(inputLen-1)) &&
-                                    input.length()>1 &&
-                                    OPERATION_SIMBOLS.contains(textBtn))
-        {
-            if(textBtn.equals("-") && !input.substring(inputLen-1).equals("-"))
-            {
-                input += textBtn;
-                tvInput.setText(input);
-                val = true;
-            }else if(input.substring(inputLen-1).equals("-"))
-            {
-                input = input.substring(0, inputLen-2) + textBtn;
-                tvInput.setText(input);
-                val = true;
-            }else
-            {
-                input = input.substring(0, inputLen-1) + textBtn;
-                tvInput.setText(input);
-                val = true;
-            }
-
-        }else if (OPERATION_SIMBOLS.contains(textBtn) &&
-                    !OPERATION_SIMBOLS.contains(input.equals("") ? "" : input.substring(inputLen-1)) &&
-                    !SPECIAL_SIMBOLS.contains(input.equals("") ? "" : input.substring(inputLen-1)) &&
-                    !"(".equals(input.equals("") ? "" : input.substring(inputLen-1)))
-        {
-            //Operadores + * / =
-            input += textBtn;
-            tvInput.setText(input);
-            val = true;
-        }else if (input.equals("") && (SPECIAL_SIMBOLS.contains(textBtn) || textBtn.equals("(")))
-        {
-            input += textBtn;
-            tvInput.setText(input);
-            val = true;
-        }else if (Pattern.matches( "[\\d+\\-/*]", input.equals("") ? "" : input.substring(inputLen-1)) &&
-                    EVEN_MORE_SPECIAL_SIMBOLS.contains(textBtn))
-        {
-            if(textBtn.equals(")"))
-            {
-
-            }
-            input += textBtn;
-            tvInput.setText(input);
-
-            val = true;
-        }
-        */
-
-        return val;
     }
 
 
-    public double calculate(String string)
+    public double calculate(String string) throws ArithmeticException, IllegalArgumentException
     {
         Expression calc = new ExpressionBuilder(string).build();
         return calc.evaluate();
